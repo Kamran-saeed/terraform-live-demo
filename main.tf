cc76495c3c191a5e7e494c62be5555f845b3520a
muhammad.kamran <muhammad.kamran@demicon.de>
2025-02-12 21:24:36 +0100

    update: add live repo

---
+++ main.tf
@@ -0,0 +1,14 @@
+# ========================================= #
+# VPC
+# ========================================= #
+module "vpc" {
+  source = "github.com/Kamran-saeed/terraform-aws-vpc"
+
+  service_name     = local.service_name
+  env              = local.env
+  vpc_name         = local.vpc_name
+  cidr             = local.cidr
+  private_subnets = local.private_subnets
+  public_subnets   = local.public_subnets
+  azs              = local.azs
+}
