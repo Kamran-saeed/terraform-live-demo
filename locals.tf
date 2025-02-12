c4ca45cea5e484a5af98846f9a81253bbeb0db64
muhammad.kamran <muhammad.kamran@demicon.de>
2025-02-12 21:29:56 +0100

    update: fix linting

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
-  region = "eu-west-1"
-  service_name     = "tfc-agent"
-  env              = "demo"
-  
-  cidr             = "172.0.0.0/22"
-  private_subnets    = ["172.0.0.0/24", "172.0.1.0/24", "172.0.2.0/24"]
-  public_subnets = ["172.0.3.0/26", "172.0.3.64/26", "172.0.3.128/26"]
-  azs              = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  
+  region       = "eu-west-1"
+  service_name = "tfc-agent"
+  env          = "demo"
+
+  cidr            = "172.0.0.0/22"
+  private_subnets = ["172.0.0.0/24", "172.0.1.0/24", "172.0.2.0/24"]
+  public_subnets  = ["172.0.3.0/26", "172.0.3.64/26", "172.0.3.128/26"]
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+
   vpc_name = "${local.env}-${local.service_name}-vpc"
 }
--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@
 module "vpc" {
   source = "github.com/Kamran-saeed/terraform-aws-vpc"
 
-  service_name     = local.service_name
-  env              = local.env
-  vpc_name         = local.vpc_name
-  cidr             = local.cidr
+  service_name    = local.service_name
+  env             = local.env
+  vpc_name        = local.vpc_name
+  cidr            = local.cidr
   private_subnets = local.private_subnets
-  public_subnets   = local.public_subnets
-  azs              = local.azs
+  public_subnets  = local.public_subnets
+  azs             = local.azs
 }
